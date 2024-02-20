dd08a539e80819857ccb73ee1e08bb2bf7007cc2
pratik-ranpariya <pratikranpariya007@gmail.com>
2024-02-20 11:14:34 +0530

    code refactor

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-// require = require('esm')(module);
 const express = require('express')
 const app = express()
 const port = 3000
 require('dotenv').config()
 const { GITHUB_ACCESS_TOKEN, GITHUB_USERNAME, GITHUB_REPO_NAME } = process.env
-// const { Octokit } = require("@octokit/rest");
 const fetch = require('node-fetch');
-// app.get('/', (req, res) => res.send('Hello World!'))
 
 const fs = require('fs');
-// const octokit = new Octokit({
-//     auth: GITHUB_ACCESS_TOKEN,// Replace with your personal access token
-//     request: {
-//         fetch: fetch
-//     }
-// });
 
 const owner = GITHUB_USERNAME;
 const repo = GITHUB_REPO_NAME;
@@ -137,61 +128,5 @@ async function createCommit(fileContent) {
       console.error('Error creating commit:', error);
     }
   }
-// Create a new commit
-// async function createCommit() {
-//     try {
-//         console.log("{{{{{{{{{{{{{{{}}}}}}}}}}}}}}");
-//       // Get the latest commit SHA for the default branch
-//       const { data: { object: { sha: latestCommitSha } } } = await octokit.git.getRef({
-//         owner,
-//         repo,
-//         ref: 'heads/gh-pages' // Change to your default branch name if it's different
-//       });
-
-//       console.log(":::::::::::::::::::latestCommitSha", latestCommitSha);
-  
-//       // Create a blob with the file content
-//       const { data: { sha: blobSha } } = await octokit.git.createBlob({
-//         owner,
-//         repo,
-//         content: fileContent,
-//         encoding: 'utf-8'
-//       });
-  
-//       // Create a new tree with the updated file
-//       const { data: { sha: newTreeSha } } = await octokit.git.createTree({
-//         owner,
-//         repo,
-//         base_tree: latestCommitSha,
-//         tree: [{
-//           path: filePath,
-//           mode: '100644',
-//           type: 'blob',
-//           sha: blobSha
-//         }]
-//       });
-  
-//       // Create a new commit
-//       const { data: { sha: newCommitSha } } = await octokit.git.createCommit({
-//         owner,
-//         repo,
-//         message: commitMessage,
-//         tree: newTreeSha,
-//         parents: [latestCommitSha]
-//       });
-  
-//       // Update the reference to point to the new commit
-//       await octokit.git.updateRef({
-//         owner,
-//         repo,
-//         ref: 'heads/gh-pages', // Change to your default branch name if it's different
-//         sha: newCommitSha
-//       });
-  
-//       console.log('Commit created successfully!');
-//     } catch (error) {
-//       console.error('Error creating commit:', error);
-//     }
-//   }
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
